98fbd62ce6d271e526fad46b15a4f0715cc9f761
Kate Thibault <kthibault@neoninc.org>
2011-03-22 20:24:54 +0000

    Changed faceted = false to edgecolors='none' to stop the Deprecation Warnings.

--- macroeco.py
+++ macroeco.py
@@ -137,12 +137,12 @@ def plot_color_by_pt_dens(x, y, radius, loglog=0, plot_obj=None):
         plot_obj.set_xscale('log')
         plot_obj.set_yscale('log')
         plot_obj.scatter(sorted_plot_data[:, 0], sorted_plot_data[:, 1],
-                         c = np.sqrt(sorted_plot_data[:, 2]), faceted=False)
+                         c = np.sqrt(sorted_plot_data[:, 2]), edgecolors='none')
         plot_obj.set_xlim(min(x) * 0.5, max(x) * 2)
         plot_obj.set_ylim(min(y) * 0.5, max(y) * 2)
     else:
         plot_obj.scatter(sorted_plot_data[:, 0], sorted_plot_data[:, 1],
-                    c = log10(sorted_plot_data[:, 2]), faceted=False)
+                    c = log10(sorted_plot_data[:, 2]), edgecolors='none')
     return plot_obj
 
 def confidence_hull(x, y, radius, confidence_int = 0.95, logscale=0, color='b',
