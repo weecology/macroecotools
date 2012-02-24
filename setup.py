d0f71a66053ca3dbb0c6a895f3974965a9e089c9
Ethan White <ethan@weecology.org>
2012-02-24 13:55:30 -0700

    Adding setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name='macroeco',
+      version='1.x-dev',
+      py_modules=['macroeco'])
+
+setup(name='macroeco_distributions',
+      version='1.x-dev',
+      py_modules=['macroeco_distributions'])
