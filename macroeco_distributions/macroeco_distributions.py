2a4a99d0cc5126328b4fcced85da2812683330fc
Xiao Xiao <xiao@weecology.org>
2014-05-14 07:59:41 -0600

    Modify the searching bound in trunc_geom_solver again for speed

--- macroeco_distributions/macroeco_distributions.py
+++ macroeco_distributions/macroeco_distributions.py
@@ -456,9 +456,8 @@ def trunc_geom_solver(ab, upper_bound):
     DIST_FROM_BOUND = 10 ** -10
     S = len(ab)
     N = sum(ab)
-    y = lambda x: N * (S-1) * (1-x) * x ** upper_bound + \
-        S * x ** upper_bound + N * (1-x) - S
-    one_minus_p = optimize.bisect(y, 0, BOUNDS[1] - DIST_FROM_BOUND, 
+    y = lambda x: (N * (S-1) * (1-x) + S) * x ** upper_bound + N * (1-x) - S
+    one_minus_p = optimize.bisect(y, 1 - S/N - DIST_FROM_BOUND, BOUNDS[1] - DIST_FROM_BOUND, 
                                   xtol = 1.490116e-16)
     return 1 - one_minus_p
 
