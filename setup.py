841bc220ba8af9f5b9c3b5ec35547e46f8104037
Ethan White <ethan@weecology.org>
2016-11-15 08:49:06 -0500

    Update classifiers in setup.py

--- setup.py
+++ setup.py
@@ -8,9 +8,14 @@ except ImportError:
 setup(name = 'macroecotools',
       version= '0.4.0',
       description = 'Tools for conducting macroecological analyses',
-      author = "Xiao Xiao, Katherine M. Thibault, David J. Harris, Elita Baldridge, and Ethan P. White",
+      author = "Xiao Xiao, Kate Thibault, David Harris, Elita Baldridge, and Ethan White",
       url = 'https://github.com/weecology/macroecotools',
       packages = ['macroecotools', 'macroeco_distributions'],
       license = 'MIT',
-      install_requires = ['numpy>=1.6', 'scipy>=0.12', 'pandas>=0.13']
+      install_requires = ['numpy>=1.6', 'scipy>=0.12', 'pandas>=0.13'],
+      classifiers=['Intended Audience :: Science/Research',
+                   'License :: OSI Approved :: MIT License',
+                   'Programming Language :: Python',
+                   'Programming Language :: Python :: 2', ],
+
 )
